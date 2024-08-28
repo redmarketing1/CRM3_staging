712b7e01ce7bd378a7ff514e47761ae31c9ce526
fxcjahid <fxcjahid3@gmail.com>
2024-08-28 21:48:28 +0600

    Changes to be committed:
        ** I have created Project Module system that is defined all related projects code stored in there
        ** In this system later helps to others developer find easily & readable
    
    	new file:   Modules/Project/Config/config.php
    	new file:   Modules/Project/Database/Seeders/MarketPlaceSeederTableSeeder.php
    	new file:   Modules/Project/Database/Seeders/PermissionTableSeeder.php
    	new file:   Modules/Project/Database/Seeders/ProjectDatabaseSeeder.php
    	new file:   Modules/Project/Http/Controllers/Company/SettingsController.php
    	new file:   Modules/Project/Http/Controllers/ProjectController.php
    	new file:   Modules/Project/Http/Controllers/SuperAdmin/SettingsController.php
    	new file:   Modules/Project/Listeners/CompanyMenuListener.php
    	new file:   Modules/Project/Listeners/CompanySettingListener.php
    	new file:   Modules/Project/Listeners/CompanySettingMenuListener.php
    	new file:   Modules/Project/Listeners/SuperAdminMenuListener.php
    	new file:   Modules/Project/Listeners/SuperAdminSettingListener.php
    	new file:   Modules/Project/Listeners/SuperAdminSettingMenuListener.php
    	new file:   Modules/Project/Providers/EventServiceProvider.php
    	new file:   Modules/Project/Providers/ProjectServiceProvider.php
    	new file:   Modules/Project/Providers/RouteServiceProvider.php
    	new file:   Modules/Project/Resources/views/company/settings/index.blade.php
    	new file:   Modules/Project/Resources/views/index.blade.php
    	new file:   Modules/Project/Resources/views/layouts/master.blade.php
    	new file:   Modules/Project/Resources/views/marketplace/index.blade.php
    	new file:   Modules/Project/Resources/views/super-admin/settings/index.blade.php
    	new file:   Modules/Project/Routes/api.php
    	new file:   Modules/Project/Routes/web.php
    	new file:   Modules/Project/composer.json
    	new file:   Modules/Project/module.json
    	new file:   Modules/Project/package.json

---
+++ Modules/Project/Listeners/CompanyMenuListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Project\Listeners;
+
+use App\Events\CompanyMenuEvent;
+
+class CompanyMenuListener
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(CompanyMenuEvent $event): void
+    {
+        $module = 'Project';
+        $menu = $event->menu;
+        $menu->add([
+            'title' => 'Project',
+            'icon' => 'home',
+            'name' => 'project',
+            'parent' => null,
+            'order' => 2,
+            'ignore_if' => [],
+            'depend_on' => [],
+            'route' => 'home',
+            'module' => $module,
+            'permission' => 'manage-dashboard'
+        ]);
+    }
+}
