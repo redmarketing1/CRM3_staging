64b9dd8bc8af7c86f8f029735ba600c24be86d60
fxcjahid <fxcjahid3@gmail.com>
2024-08-31 16:05:28 +0600

    Changes to be committed: 	modified:   .timetracker 	modified:   Modules/Project/Resources/views/project/widget/project_setting_dropdown.blade.php

--- Modules/Project/Resources/views/project/widget/project_setting_dropdown.blade.php
+++ Modules/Project/Resources/views/project/widget/project_setting_dropdown.blade.php
@@ -1,13 +1,12 @@
 @permission('project setting')
     @if (!empty($projectLabel['project_status']))
         <div class="col-sm-auto">
-            {{-- Optional: If you want to show the current status as a button, uncomment the following lines --}}
-            {{-- 
+
             <button class="btn btn-xs btn-primary text-white btn-icon-only width-auto dropdown-toggle" type="button"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {{ $project->status_data->name ?? '' }}
             </button>
-            --}}
+
             <div class="dropdown-menu">
                 @foreach ($projectLabel['project_status'] as $status)
                     @if ($status->id == env('PROJECT_STATUS_CLIENT'))
