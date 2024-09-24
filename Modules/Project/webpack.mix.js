7a0cdcf03da4b6c731ee2f93686b8a65a8dc84d6
fxcjahid <fxcjahid3@gmail.com>
2024-09-24 11:13:11 +0600

    Changes to be committed: 	modified:   .gitignore 	modified:   .timetracker 	modified:   Modules/Project/Providers/ProjectServiceProvider.php 	deleted:    Modules/Project/Resources/assets/css/project.index.css 	modified:   Modules/Project/webpack.mix.js 	modified:   bootstrap/cache/packages.php 	modified:   bootstrap/cache/services.php 	modified:   public/assets/css/project.index.css 	modified:   public/assets/js/project.index.js

--- Modules/Project/webpack.mix.js
+++ Modules/Project/webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix');
 
 mix.js(`${__dirname}/Resources/assets/js/project.index.js`, `/assets/js/project.index.js`)
-    .css(`${__dirname}/Resources/assets/css/project.index.css`, `/assets/css/project.index.css`);
+    .sass(`${__dirname}/Resources/assets/css/project.index.scss`, `/assets/css/project.index.css`);
