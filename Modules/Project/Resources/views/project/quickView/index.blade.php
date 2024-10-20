82a16969bd2f78724b56ab048e12184709223eae
fxcjahid <fxcjahid3@gmail.com>
2024-10-20 23:19:23 +0600

    Changes to be committed: 	modified:   Modules/Project/Resources/assets/js/project.quickView.js 	modified:   Modules/Project/Resources/views/project/quickView/index.blade.php 	modified:   public/assets/js/project.quickView.js

--- Modules/Project/Resources/views/project/quickView/index.blade.php
+++ Modules/Project/Resources/views/project/quickView/index.blade.php
@@ -1,4 +1,10 @@
 <link rel="stylesheet" href="{{ asset('assets/css/project.quickView.css') }}">
+<script>
+    $(document).ready(function() {
+        var projectID = '{{ $project->id }}';
+        var supportedFormats = '{{ getAdminAllSetting()['local_storage_validation'] }}';
+    });
+</script>
 
 @include('project::project.show.utility.header')
 @include('project::project.show.utility.dashboard')
@@ -19,13 +25,5 @@
 @include('project::project.show.section.milestone')
 
 
-<script src="{{ asset('assets/js/project.quickView.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/exif-js"></script>
-
-<script>
-    $(document).ready(function() {
-        const projectID = '{{ $project->id }}';
-        const csrfToken = $('meta[name="csrf-token"]').attr('content');
-        const supportedFormats = '{{ getAdminAllSetting()['local_storage_validation'] }}';
-    });
-</script>
+<script src="{{ asset('assets/js/project.quickView.js') }}"></script>
