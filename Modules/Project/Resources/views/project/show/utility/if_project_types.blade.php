426e835c526445d1dfbf72ae6e09a60f378dfa72
fxcjahid <fxcjahid3@gmail.com>
2024-10-27 15:12:57 +0000

    from server

--- Modules/Project/Resources/views/project/show/utility/if_project_types.blade.php
+++ Modules/Project/Resources/views/project/show/utility/if_project_types.blade.php
@@ -1,11 +1,12 @@
 <div class="col-xxl-12 card-container mt-5 mb-5">
+    @include('project::project.show.card.description')
     @permission('team member view')
         @include('project::project.show.card.team_members')
     @endpermission
     {{-- @include('project::project.show.card.client_share') --}}
 
     @include('project::project.show.card.appointments')
-    @include('project::project.show.card.description')
+    
 
     @include('project::project.show.card.project_labels')
     @include('project::project.show.card.contact')
