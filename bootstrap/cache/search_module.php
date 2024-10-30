4557f173d991e5e3d1033eaf4ef70c4d84f39e49
Asad Zaman <AsadZaman177@gmail.com>
2024-10-30 22:09:58 +0500

    bootstapp cache

---
+++ bootstrap/cache/search_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Search\\Providers\\SearchServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Search\\Providers\\SearchServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
